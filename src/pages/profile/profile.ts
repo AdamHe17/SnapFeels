725ed9e3c20ab593d55b2ace208e71d7d34ddc3c
Adam He <shuaihe2018@u.northwestern.edu>
2017-10-08 04:07:18 -0500

    second graph working too

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -30,7 +30,7 @@ export class ProfilePage {
   data: any;
 
   constructor(public navCtrl: NavController, public firestoreProvider: FirestoreProvider) {
-    this.selectedFeeling = "Happy";
+    this.selectedFeeling = "happiness";
     this.feelingToColor = {
       anger: 'rgba(170, 57, 57, 1)',
       contempt: 'rgba(24, 45, 8, 1)',
@@ -62,7 +62,6 @@ export class ProfilePage {
     var barDatasets = [];
     var labels = [];
     this.data.subscribe(d => {
-      labels = d.map(_d => new Date(_d['timestamp']).toISOString());
       for (var i = 0; i < 8; i++) {
         var feeling = Object.keys(this.feelingToColor)[i];
         var data = d.map(_d => _d['scores'][feeling]);
@@ -75,12 +74,10 @@ export class ProfilePage {
         });
       }
 
-      console.log(labels, barDatasets);
-
       this.stackedBarChart = new Chart(this.stackedBarCanvas.nativeElement, {
         type: 'bar',
         data: {
-          labels: labels,
+          labels: d.map(_d => new Date(_d['timestamp']).toISOString()),
           datasets: barDatasets
         },
         options: {
@@ -106,57 +103,48 @@ export class ProfilePage {
           }
         }
       });
-    });
 
-    this.lineChart = new Chart(this.lineCanvas.nativeElement, {
-      type: 'line',
-      data: {
-        labels: ["January", "February", "March", "April", "May", "June", "July"],
-        datasets: [
-          {
-            label: "dead",
-            fill: false,
-            lineTension: 0.1,
-            borderColor: "rgba(150,192,100,1)",
-            borderCapStyle: 'butt',
-            borderDash: [],
-            borderDashOffset: 0.0,
-            borderJoinStyle: 'miter',
-            pointBorderColor: "rgba(150,192,100,1)",
-            pointBackgroundColor: "#fff",
-            pointBorderWidth: 3,
-            pointHoverRadius: 5,
-            pointHoverBackgroundColor: "rgba(150,192,100,1)",
-            pointHoverBorderColor: "rgba(220,220,220,1)",
-            pointHoverBorderWidth: 2,
-            pointRadius: 1,
-            pointHitRadius: 10,
-            data: [65, NaN, 80, 81, 56, 55, 40],
-            spanGaps: true,
-          },
-          {
-            label: this.selectedFeeling,
-            fill: false,
-            lineTension: 0.1,
-            borderColor: "rgba(75,192,192,1)",
-            borderCapStyle: 'butt',
-            borderDash: [],
-            borderDashOffset: 0.0,
-            borderJoinStyle: 'miter',
-            pointBorderColor: "rgba(75,192,192,1)",
-            pointBackgroundColor: "#fff",
-            pointBorderWidth: 1,
-            pointHoverRadius: 5,
-            pointHoverBackgroundColor: "rgba(75,192,192,1)",
-            pointHoverBorderColor: "rgba(220,220,220,1)",
-            pointHoverBorderWidth: 2,
-            pointRadius: 1,
-            pointHitRadius: 10,
-            data: [12, 24, 34, 52, 12, NaN, 34],
-            spanGaps: true,
+      this.lineChart = new Chart(this.lineCanvas.nativeElement, {
+        type: 'line',
+        data: {
+          labels: d.map(_d => new Date(_d['timestamp']).toISOString()),
+          datasets: [
+            {
+              label: this.selectedFeeling,
+              fill: false,
+              lineTension: 0.1,
+              borderColor: this.feelingToColor[this.selectedFeeling],
+              borderCapStyle: 'butt',
+              borderDash: [],
+              borderDashOffset: 0.0,
+              borderJoinStyle: 'miter',
+              pointBorderColor: this.feelingToColor[this.selectedFeeling],
+              pointBackgroundColor: "#fff",
+              pointBorderWidth: 3,
+              pointHoverRadius: 5,
+              pointHoverBackgroundColor: this.feelingToColor[this.selectedFeeling],
+              pointHoverBorderColor: "rgba(220,220,220,1)",
+              pointHoverBorderWidth: 2,
+              pointRadius: 1,
+              pointHitRadius: 10,
+              data: d.map(_d => _d['scores'][this.selectedFeeling]),
+              spanGaps: true,
+            }
+          ],
+          options: {
+            scales: {
+              xAxes: [{
+                type: 'time',
+                time: {
+                  displayFormats: {
+                    day: 'MM/DD'
+                  }
+                }
+              }]
+            }
           }
-        ]
-      }
+        }
+      });
     });
   }
 }
