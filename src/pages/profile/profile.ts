c6c7dd4ddc5141181471eb6c7ad2d49f4ab9ba8c
xunil17 <seancye@gmail.com>
2017-10-07 23:45:08 -0500

    working line chart example

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -1,8 +1,8 @@
-import { Component } from '@angular/core';
+import { Component,ViewChild } from '@angular/core';
 import { IonicPage, NavController, NavParams, ViewController } from 'ionic-angular';
 import { User } from "../../models/user";
-import { AngularFireAuth } from 'angularfire2/auth'
-
+import { AngularFireAuth } from 'angularfire2/auth';
+import { Chart } from 'chart.js';
 
 /**
  * Generated class for the ProfilePage page.
@@ -17,5 +17,48 @@ import { AngularFireAuth } from 'angularfire2/auth'
   templateUrl: 'profile.html',
 })
 export class ProfilePage {
+
+    @ViewChild('lineCanvas') lineCanvas;
+
+    lineChart: any;
+
+    constructor(public navCtrl: NavController) {
+    }
+
+    ionViewDidLoad() {
+        this.lineChart = new Chart(this.lineCanvas.nativeElement, {
+
+            type: 'line',
+            data: {
+                labels: ["January", "February", "March", "April", "May", "June", "July"],
+                datasets: [
+                    {
+                        label: "My First dataset",
+                        fill: false,
+                        lineTension: 0.1,
+                        backgroundColor: "rgba(75,192,192,0.4)",
+                        borderColor: "rgba(75,192,192,1)",
+                        borderCapStyle: 'butt',
+                        borderDash: [],
+                        borderDashOffset: 0.0,
+                        borderJoinStyle: 'miter',
+                        pointBorderColor: "rgba(75,192,192,1)",
+                        pointBackgroundColor: "#fff",
+                        pointBorderWidth: 1,
+                        pointHoverRadius: 5,
+                        pointHoverBackgroundColor: "rgba(75,192,192,1)",
+                        pointHoverBorderColor: "rgba(220,220,220,1)",
+                        pointHoverBorderWidth: 2,
+                        pointRadius: 1,
+                        pointHitRadius: 10,
+                        data: [65, 59, 80, 81, 56, 55, 40],
+                        spanGaps: false,
+                    }
+                ]
+            }
+
+        });
+
+    }
 
 }
